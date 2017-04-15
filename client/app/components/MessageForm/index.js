0cb5f3ab1ab629c6fdc99bd10e7e43960db106e5
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-04-15 14:22:16 +0900

    step 8

---
+++ client/app/components/MessageForm/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MessageForm extends Component {
+  render() {
+    return (
+      <div>
+        <textarea rows="4" cols="40" />
+        <input type="button" value="register" />
+      </div>
+    )
+  }
+}
+
+export default MessageForm
